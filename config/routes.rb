7e143e43fe1b39933febcd2d7a3290ad52c39a16
Gustavo Dias <gust.webdev@gmail.com>
2017-06-21 07:28:59 -0300

    Refatoração da Home page e adição dos códigos de estilização do layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home' #Root define a página inicial da aplicação. ex: localhost:3000/
+
+  get 'about' => 'pages#about'
+
 end
